575c64e557639f6de41f6e7bdfe3afb7e65ffe20
PranshuDuke <singhpranshu58@gmail.com>
2021-12-01 22:46:20 +0530

    Add Trip Implemented

---
+++ app/src/main/java/com/example/finalprojectv1/AllTrips.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojectv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllTrips : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_trips)
+    }
+}
