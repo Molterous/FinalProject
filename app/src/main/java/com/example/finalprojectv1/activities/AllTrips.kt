c78539a46372e64f6bd58e6ff304ba1cf7247872
Molterous <aakashgarud007@gmail.com>
2022-03-07 23:52:11 +0530

    Map Activity part 1

--- app/src/main/java/com/example/finalprojectv1/activities/AllTrips.kt
+++ app/src/main/java/com/example/finalprojectv1/activities/AllTrips.kt
@@ -2,20 +2,19 @@
 
 package com.example.finalprojectv1.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalprojectv1.ProfileActivity
 import com.example.finalprojectv1.R
 import com.example.finalprojectv1.databinding.ActivityAllTripsBinding
-import com.example.finalprojectv1.databinding.ActivityMainBinding
+import com.example.finalprojectv1.utils.OnIntentReceived
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -33,6 +32,8 @@ class AllTrips : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var userArrayList: ArrayList<FetchTrips>
     private lateinit var tempArrayList:ArrayList<FetchTrips>
+    private lateinit var mIntentReceived : OnIntentReceived
+    private lateinit var holder: tripAdapter.MyViewHolder
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,7 +64,7 @@ class AllTrips : AppCompatActivity() {
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_item,menu)
         val item=menu?.findItem(R.id.search_action)
         val searchView=item?.actionView as SearchView
@@ -129,6 +130,7 @@ class AllTrips : AppCompatActivity() {
                    // userRecyclerview.adapter = tripAdapter(this@AllTrips,userArrayList)
 
                     userRecyclerview.adapter = tripAdapter(this@AllTrips,tempArrayList)
+                    mIntentReceived = userRecyclerview.adapter as tripAdapter
 
 
                 }
@@ -140,6 +142,25 @@ class AllTrips : AppCompatActivity() {
 
         })
     }
+
+    var launchSomeActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            var lat = "0.0"
+            var long = "0.0"
+            lat = result.data?.extras?.getString("Lat").toString()
+            long = result.data?.extras?.getString("Long").toString()
+            mIntentReceived.onIntent(lat, long, holder)
+        }
+    }
+
+    fun openYourActivity(holder: tripAdapter.MyViewHolder) {
+        this.holder = holder
+        val intent = Intent(this, MapTriggerActivity::class.java)
+        launchSomeActivity.launch(intent)
+    }
+
+
 }
 
 
